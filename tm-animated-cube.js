76b4f7b18f24622ec9ed74210dd509efbe218a27
Tim McMaster <tim@mcmaster.id.au>
2018-10-14 17:43:33 +1100

    added power saving render only when needed.

--- tm-animated-cube.js
+++ tm-animated-cube.js
@@ -1,7 +1,11 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import {IronResizableBehavior} from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js';
+
 import 'jquery/dist/jquery.js';
 import * as THREE from 'three';
 import './lib/jquery.cube.threejs.js';
+
 window.THREE = THREE;
 
 /**
@@ -12,49 +16,107 @@ window.THREE = THREE;
  * @polymer
  * @demo demo/index.html
  */
-class TmAnimatedCube extends PolymerElement {
-  static get template() {
-    return html`
+class TmAnimatedCube extends mixinBehaviors([IronResizableBehavior], PolymerElement) {
+    static get template() {
+        return html`
       <style>
         :host {
           display: block;
-          width: 80vw;
-          height: 80vw;
         }
+        
         div {
             width: 100%;
             height: 100%;
-            border: solid red 1px;
+            border: solid blue 1px;
             box-sizing: border-box;
         }
       </style>
-      <h2>Hello [[prop1]]!</h2>
-      <div id="cube" ></div>
-      <!--<div id="cube" data-moves="(U R U' R') (U R U' R') (U R U' R') (U R U' R') (U R U' R') (U R U' R')"></div>-->
-	    <h3>-----------------------------</h3>
+      <div id="container">
+        <div id="cube" ></div>
+      </div>
+      
+     
     `;
-  }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'This is a test',
-      },
-    };
-  }
+    }
 
-  ready() {
-      super.ready();
-      // const cube = this.shadowRoot.querySelector('#cube');
-      // console.log(cube);
+    static get properties() {
+        return {
+            scramble: {
+                type: String
+            },
+            moves: {
+                type: String,
+                value: '',
+                observer: '_movesChanged'
+            }
+        };
+    }
 
+    _movesChanged(moves) {
+        console.log('Moves: ', moves);
+    }
 
-      const c = $(this.$.cube).cube();
-      c.move("R U R' U'");
-      c.move("U R U' R'");
+    move(moves) {
+        this.moves += ' ' + moves;
+        this.cube.move(moves);
+    }
 
+    onIronResizeHold() {
+        console.log('tm-aspect-div has resized:');
 
-  }
+        const self = this;
+        if (!this.resizeTimeout) {
+            this._rebuild();
+            this.resizeTimeout = setTimeout(function () {
+                self.resizeTimeout = null;
+                self._rebuild();
+            }, 200);
+        }
+    }
+
+    onIronResize() {
+        console.log('tm-aspect-div has resized:');
+
+        const self = this;
+        self.resizeCounter++;
+        setTimeout(function () {
+            self.resizeCounter--;
+            if (self.resizeCounter < 1) {
+                self._rebuild();
+            }
+        }, 500);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        console.log('Listening for Resize Events')
+        this.addEventListener('iron-resize', this.onIronResize.bind(this));
+    }
+
+    _rebuild() {
+        if (!this.resizing) {
+            this.resizing = true;
+            console.log('Rebuilding...');
+            $(this.cube).detach();
+            $(this.cube).remove();
+            const div = document.createElement('div');
+            this.shadowRoot.querySelector('#container').appendChild(div);
+            this.cube = $(div).cube({
+                scramble: this.moves
+            });
+            this.resizing = false;
+        }
+    }
+
+    ready() {
+        super.ready();
+        this.resizing = false;
+        this.resizeCounter=0;
+        this.moves = this.scramble;
+        this.cube = $(this.$.cube).cube({
+            scramble: this.scramble
+        });
+    }
 }
 
 window.customElements.define('tm-animated-cube', TmAnimatedCube);
